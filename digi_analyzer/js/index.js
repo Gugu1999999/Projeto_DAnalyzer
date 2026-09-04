e93cf577db169b43660f8bd8599415df4a2c7742
Cezar Augusto <ca067821@gmail.com>
2026-09-04 10:17:45 -0300

    Mudança de palheta e bug fix

--- digi_analyzer/js/index.js
+++ digi_analyzer/js/index.js
@@ -1,3 +1,4 @@
+
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
---
+++ exercicioofthalles.js
@@ -0,0 +1,9 @@
+const anthoiny = 'gatão';
+var x;
+const ValorInteiro = 10;
+console.log(typeof ValorInteiro);
+console.log(typeof anthoiny);
+let DoisValores = (20,30);
+console.log(Sum(DoisValores));
+let f = (3 * x) - 15
+let fdx = ((-1) * (-1)) - 4 * (-1) * 5;
